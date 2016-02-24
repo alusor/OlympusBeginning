45fed71967e554c9d854c9bc3063ea56c61ad84c
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-02-24 13:51:43 -0600

    Se definieron atriutos principales

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Transform))]
 public class PlayerController : MonoBehaviour {
+    //Propiedades del jugador
+    private Transform t;
+    [SerializeField]
+    private Rigidbody2D r;
+    [SerializeField]
+    public float vSpeed;
+    [SerializeField]
+    public float hSpeed;
+    private bool isJump;
 
-	// Use this for initialization
-	void Start () {
-	
+    //Atributos de colision
+    public LayerMask Mask;
+    public GameObject groundCollider;
+    public float radiusGroundCollider;
+
+
+
+    // Use this for initialization
+    void Start () {
+        r = this.GetComponent<Rigidbody2D>();
+        t = this.GetComponent<Transform>();
 	}
-	
+    void FixedUpdate() { }
 	// Update is called once per frame
 	void Update () {
-	
+        MovePlayer();
 	}
+    void MovePlayer() { }
 }
