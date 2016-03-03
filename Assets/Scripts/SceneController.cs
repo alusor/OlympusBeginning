dc91c699d962b59e09654c56645bd66bc7fa2f8e
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-03-03 01:28:30 -0600

    SceneManagerUpdate

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void changeScene(string name) {
+        SceneManager.LoadScene(name);
+    }
 }
