f9ac8cd5031674a9e61cc97c47b9cdeb22c8c2e7
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-03-25 19:30:00 -0600

    Poder Modificar el personaje

---
+++ Assets/Scripts/SetItem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetItem : MonoBehaviour {
+    public Transform head;
+    public Transform hans;
+
+    public Sprite[] item;
+	// Use this for initialization
+	void Start () {
+        head.gameObject.AddComponent<SpriteRenderer>();
+        hans.gameObject.AddComponent<SpriteRenderer>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void setComponent(int id) {
+        switch (id) {
+            case 1:
+                head.gameObject.GetComponent<SpriteRenderer>().sprite = item[0];
+                head.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
+                break;
+            case 2:
+                break;
+            case 3:
+                head.gameObject.GetComponent<SpriteRenderer>().sprite = item[2];
+                head.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
+                break;
+            case 4:
+                hans.gameObject.GetComponent<SpriteRenderer>().sprite = item[3];
+                hans.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
+                break;
+            case 5:
+                hans.gameObject.GetComponent<SpriteRenderer>().sprite = item[4];
+                hans.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
