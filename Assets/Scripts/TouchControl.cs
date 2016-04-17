c5c2339a1472dc140d2deaf08846f7a28b6339db
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-04-17 02:51:22 -0500

    Intentando agregar InputTouchs

---
+++ Assets/Scripts/TouchControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Touch a in Input.touches){
+			Debug.Log(a.position.x);
+		}
+		/*if(Input.touchCount>0)
+			Debug.Log(Input.GetTouch(0).position.x);*/
+	}
+}
