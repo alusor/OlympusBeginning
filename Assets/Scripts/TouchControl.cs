48e69d00803e4b4df1f15db5802008bb74fe5d22
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-04-17 04:29:02 -0500

    Agregado el control Touch

--- Assets/Scripts/TouchControl.cs
+++ Assets/Scripts/TouchControl.cs
@@ -2,17 +2,38 @@
 using System.Collections;
 
 public class TouchControl : MonoBehaviour {
-
 	// Use this for initialization
+	PlayerController p;
 	void Start () {
-	
+	p =  FindObjectOfType<PlayerController>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		foreach(Touch a in Input.touches){
-			Debug.Log(a.position.x);
+			//Debug.Log(a.position.x);
+			switch (a.phase)
+			{
+
+
+				default:
+				break;
+			}
+			Vector3 te = Camera.main.ScreenToWorldPoint(a.position);
+			if(p.transform.position.x>te.x){
+				Debug.Log("Izquierda");
+			}
+			else{
+					Debug.Log("Derecha");
+				}
+				//Debug.Log(a.deltaPosition);
+				if(a.deltaPosition.y>2f){
+					Debug.Log("Arriba");
+				}else if(a.deltaPosition.y<-2f){
+					Debug.Log("Ataque");
+				}
 		}
+
 		/*if(Input.touchCount>0)
 			Debug.Log(Input.GetTouch(0).position.x);*/
 	}
